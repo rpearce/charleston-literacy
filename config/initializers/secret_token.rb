0cd4b8e481c7b31da5c981bb9075d2dbd0216502
Robert Pearce <robertwaltonpearce@gmail.com>
2011-11-08 21:40:17 -0500

    First commit for CMS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CharlestonLiteracy::Application.config.secret_token = '16d0e9d664b8d7d5d5b1517e2b6cd67cc298e62ec2e074809a50b5537c132d5a2dddeedec6ab7aa7ea455f000d9e62446d83b9552584c5861ce4e2f4752829ab'
